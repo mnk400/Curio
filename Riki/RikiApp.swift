3f2070e3043a5ee4ea39934b37f822a59ca3e65e
Manik Kumar <mnk_400@icloud.com>
2025-05-01 22:22:21 -0700

    Initial Commit

---
+++ Riki/RikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RikiApp.swift
+//  Riki
+//
+//  Created by Manik Kumar on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct RikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RikiTests/RikiTests.swift
@@ -0,0 +1,17 @@
+//
+//  RikiTests.swift
+//  RikiTests
+//
+//  Created by Manik Kumar on 5/1/25.
+//
+
+import Testing
+@testable import Riki
+
+struct RikiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
